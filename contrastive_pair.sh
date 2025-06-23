93574bf693b59087815d44a376953ca3cab15791
wsad1232885 <wsad1232885@gmail.com>
2025-06-23 12:53:47 +0800

    刪掉用不到的腳本

--- contrastive_pair.sh
+++
@@ -1,4 +0,0 @@
-python src/dataset/augment_and_preprocess_pair.py --type train --encoding label
-python src/dataset/augment_and_preprocess_pair.py --type test --encoding label
-python src/dataset/augment_and_preprocess_pair.py --type train --encoding onehot
-python src/dataset/augment_and_preprocess_pair.py --type test --encoding onehot
